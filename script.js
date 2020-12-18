96b9493e4b85797cdabc9f3eadf4f1bde63cb99a
Elektra Babian <babianelektra@gmail.com>
2020-12-17 20:19:32 -0500

    :added styling to input, squares, and brush

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('hello world');
+// console.log('hello world');
+
+const setColor = document.querySelector('#set-color');
+const colorInput = document.querySelector('#color-input');
+const brush = document.querySelector('.brush');
+const body = document.querySelector('body');
+
+
+// console.log(colorInput.innerText);
+
+setColor.addEventListener('click', changeColor);
+
+function changeColor(event) {
+  event.preventDefault();
+  brush.style.background = colorInput.value;
+}
+
+for(let i = 0; i < 10000; i++) {
+  let newDiv = document.createElement('div');
+  newDiv.classList.add('square');
+  body.appendChild(newDiv);
+}
+
+body.addEventListener('mouseover', paint);
+
+function paint(event) {
+  event.preventDefault();
+  if (event.target.classList.contains('square')){
+    event.target.style.background = brush.style.background;
+  }
+}
+// console.log(body);
